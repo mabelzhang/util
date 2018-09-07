987dfe40edfc594b1ef9d9bb5ac50e9caea9af56
mabel.m.zhang@gmail.com <mabel.m.zhang@gmail.com>
2018-09-07 01:06:36 -0400

    Ray-tracing occlusion check finished implementation. Visualized cloud and ray as arrow in RViz. Red if occlusion, green else. Simple test case works.

--- util/include/util/pcl_raytrace_util.h
+++ util/include/util/pcl_raytrace_util.h
@@ -17,10 +17,12 @@
 
 // Only used for RViz visualization
 #include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
 
 // Local
 #include <util/lin_alg_util.h>  // project_pts_onto_line()
 #include <util/pcd_util.h>  // publish_cloud()
+#include <util/skeleton.h>  // create_marker()
 
 
 //#include <iostream>
@@ -35,12 +37,15 @@ class RayTracer
 
     bool vis_;
     ros::NodeHandle * nh_;
+    std::string frame_id_;
+    ros::Publisher vis_pub_;
 
 
   public:
 
     RayTracer (pcl::PointCloud <pcl::PointXYZ>::Ptr & input_cloud,
-      float resolution, bool vis=false, ros::NodeHandle * nh=NULL)
+      float resolution, bool vis=false, ros::NodeHandle * nh=NULL,
+      std::string frame_id="/world")
     {
       octree_ = pcl::octree::OctreePointCloudSearch <pcl::PointXYZ>::Ptr (
         new pcl::octree::OctreePointCloudSearch <pcl::PointXYZ> (resolution));
@@ -49,10 +54,16 @@ class RayTracer
 
       vis_ = vis;
       nh_ = nh;
+      frame_id_ = frame_id;
 
       if (vis_)
       {
-        publish_cloud (input_cloud, "/world", "cloud", *nh_);
+        vis_pub_ = nh_ -> advertise <visualization_msgs::Marker> (
+          "visualization_marker", 5);
+
+        // This uses a publisher of different type (pcl::PointCloud), so no
+        //   conflict with visualization_msgs::Marker publisher.
+        publish_cloud (input_cloud, frame_id_, "cloud", *nh_);
       }
     }
 
@@ -78,12 +89,12 @@ class RayTracer
     //   the given point. Else returns false (point is in front of, or at
     //   intersection; point cloud does not occlude the given point).
     bool raytrace_occlusion_test (Eigen::Vector3f origin,
-      Eigen::Vector3f endpoint)
+      Eigen::Vector3f endpt)
     {
       pcl::octree::OctreePointCloudSearch <pcl::PointXYZ>::
         AlignedPointTVector vx_centers;
       // Ray trace. Find centers of voxels that the ray intersects
-      octree_ -> getIntersectedVoxelCenters (origin, endpoint - origin,
+      octree_ -> getIntersectedVoxelCenters (origin, endpt - origin,
         vx_centers);
 
 
@@ -108,19 +119,11 @@ class RayTracer
       // Return value is distance from starting point of ray. Positive means
       //   along ray, negative means in opposite direction of ray.
       Eigen::VectorXf proj_vxs;
-      project_pts_onto_line (origin, endpoint, vx_centers_eg,
+      project_pts_onto_line (origin, endpt, vx_centers_eg,
         proj_vxs);
 
       // Distance of endpoint of ray from origin of ray
-      float proj_endpoint = (endpoint - origin).norm ();
-
-
-      // TODO: Visualize in RViz
-      if (vis_)
-      {
-
-      }
-
+      float proj_endpt = (endpt - origin).norm ();
 
       // If distance from ray origin to intersection in point cloud <
       //   distance from ray origin to the given point, then the given point
@@ -129,10 +132,62 @@ class RayTracer
       // When there are multiple intersections in the cloud, as long as the
       //   given point is behind at least one point in the cloud, it is
       //   occluded by the cloud.
-      if ((proj_vxs.array () < proj_endpoint).count () > 0)
-        return true;
+      bool occluded = false;
+      if ((proj_vxs.array () < proj_endpt).count () > 0)
+        occluded = true;
       else
-        return false;
+        occluded = false;
+
+
+      // Visualize ray and occlusion in RViz. If occluded (i.e. ray goes
+      //   through cloud), red ray. Else (ray goes through free space), green.
+      if (vis_)
+      {
+        float r = 0.0, g = 0.0, b = 0.0;
+        // Color ray red, to indicate it hit something
+        if (occluded)
+        {
+          r = 1.0;
+          g = 0.0;
+          b = 0.0;
+        }
+
+        // Color ray green, to indicate it went through free space.
+        else
+        {
+          r = 0.0;
+          g = 1.0;
+          b = 0.0;
+        }
+
+        visualization_msgs::Marker marker_ray;
+        create_marker (visualization_msgs::Marker::ARROW, frame_id_, 0,
+          // sx: shaft diameter, sy: head diameter, sz if != 0: head length
+          0, 0, 0, r, g, b, 0.8, 0.01, 0.02, 0,
+          marker_ray, "ray", 1, 0, 0, 0, 0);
+
+        // Start point of ray
+        geometry_msgs::Point p1;
+        p1.x = origin [0];
+        p1.y = origin [1];
+        p1.z = origin [2];
+        marker_ray.points.push_back (p1);
+
+        // End point of ray
+        geometry_msgs::Point p2;
+        p2.x = endpt [0];
+        p2.y = endpt [1];
+        p2.z = endpt [2];
+        marker_ray.points.push_back (p2);
+
+        for (int i = 0; i < 5; i ++)
+        {
+          vis_pub_.publish (marker_ray);
+          ros::Rate (10).sleep ();
+        }
+      }
+
+      return occluded;
     }
 };
 
